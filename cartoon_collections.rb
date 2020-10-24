f5071e32d048e3cd4d3ef73f783fed004df717ae
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 11:59:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,8 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each 
+  array.each do |character|
+    p 
 end
 
 def list_dwarves(array)
