a619fbc5adfed97d77d40b9ecab788fc137f0933
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 12:03:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |character, index|
-    p "#{index}. #{character}"
+    p "#{index + 1}. #{character}"
   end
 end
